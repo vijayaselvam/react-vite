f36c5b3987b8113bc9581ef4f9c57af15061e45c
vijayaselvam <vijayaselvam@users.noreply.github.com>
2024-04-10 16:52:00 +0530

    Add my name

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <h1>Vijaya Selvam Ceemon</h1>
         </p>
       </div>
       <p className="read-the-docs">
